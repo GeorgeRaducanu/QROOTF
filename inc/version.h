0fa0914609e366740639116c45e9a89b2e1476e7
GeorgeRaducanu <georgecristian2002@gmail.com>
2025-07-26 17:22:39 +0300

    Added version.h for keeping track of versioning

---
+++ inc/version.h
@@ -0,0 +1,16 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+#define QROOTF_VERSION_MAJOR 0
+#define QROOTF_VERSION_MINOR 1
+#define QROOTF_VERSION_PATCH 0
+
+// Helper macros to create version string from components
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
+
+#define QROOTF_VERSION_STRING TOSTRING(QROOTF_VERSION_MAJOR) "." \
+                            TOSTRING(QROOTF_VERSION_MINOR) "." \
+                            TOSTRING(QROOTF_VERSION_PATCH)
+
+#endif // VERSION_H
